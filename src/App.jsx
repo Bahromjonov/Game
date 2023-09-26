588f2252269fc1ebc5a0f8505ed0d320869ab6ed
Yaxyobek03 <yaxyobekbahromjonov44@gmail.com>
2023-09-26 15:20:09 +0500

    add react router 6.14

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import Router from 'router-dom'
-
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import MainRouter from './layouts/MainRouter'
 import Home from './pages/Home'
 
+
 function App() {
-<Router/>
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<MainRouter />}>
+        <Route path='/' element={<Home />} />
+      </Route>
+    )
+  )
 
   return (
-    <div className='bg-slate-100 min-h-screen'>
-      <Home />
-    </div>
+    <RouterProvider router={router} />
   )
 }
 
